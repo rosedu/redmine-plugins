9fa607df9287a6c28552e703f57ff28a20377b5a
Tudor Cornea <tudor.cornea@gmail.com>
2011-07-11 21:17:14 +0300

    Before implementing downloadaction

--- redmine_uploads/app/models/upload_form.rb
+++ redmine_uploads/app/models/upload_form.rb
@@ -1,7 +1,31 @@
 class UploadForm < ActiveRecord::Base
   unloadable
-  belongs_to :project, :polymorphic => true
+  belongs_to :project
+
   has_many :attachments, :foreign_key => "container_id"
   acts_as_attachable :delete_permission => :manage_documents
   acts_as_activity_provider :find_options => {:include => :project}
+  
+  validates_presence_of :project, :title
+  validates_length_of :title, :maximum => 60
+  
+  named_scope :visible, lambda {|*args| {:include => :project, :conditions => Project.allowed_to_condition(args.shift || User.current, :view_documents, *args) } }
+
+  def visible?(user=User.current)
+    !user.nil? && user.allowed_to?(:view_documents, project)
+  end
+
+  def after_initialize
+    if new_record?
+      self.category ||= DocumentCategory.default
+    end
+  end
+
+  def updated_on
+    unless @updated_on
+      a = attachments.find(:first, :order => 'created_on')
+      @updated_on = (a && a.created_on) || created_on
+    end
+    @updated_on
+  end
 end
